8e3eff67103aa41648c972c3f453c7a63f845dde
Ashish@8710 <gsashishemail@gmail.com>
2023-01-08 21:18:18 +0530

    modal component

---
+++ topic/modalcomponent/README.md
@@ -0,0 +1,31 @@
+# modalcomponent
+class my props
+normal props string directly write.
+if any variable then :propName
+event handle
+modifier event : @click.self and @click.shift
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ topic/modalcomponent/src/components/as-modal.vue
@@ -0,0 +1,76 @@
+<template>
+  <div v-show="showModal">
+    <div class="backdrop" @click.self="closeModal">
+      <div
+        class="modal"
+        :class="{
+          danger: theme === modalTheme.danger,
+          success: theme === modalTheme.success,
+          neutral: theme === modalTheme.neutral,
+        }"
+      >
+        <slot>default slot</slot>
+        <div class="actions">
+          <slot name="links"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "asModel",
+  props: ["showModal", "theme"],
+  data() {
+    return {
+      modalTheme: { danger: "danger", success: "success", neutral: "neutral" },
+    };
+  },
+  methods: {
+    closeModal() {
+      console.log("ran ", this.showModal, close);
+      this.$emit("close");
+      console.log("ran ", this.showModal, close);
+    },
+  },
+};
+</script>
+<style>
+.backdrop {
+  position: absolute;
+  z-index: 1000;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.modal {
+  background-color: white;
+  padding: 10px;
+  border-radius: 5px;
+  min-width: 300px;
+  min-height: 200px;
+}
+.modal.neutral {
+  background-color: skyblue;
+}
+.modal.success {
+  background-color: greenyellow;
+}
+.modal.danger {
+  background-color: red;
+}
+.actions a {
+  color: white;
+  text-decoration: none;
+  padding: 5px 10px;
+  border: 1px solid white;
+}
+</style>
